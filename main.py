940aa0e6ef2cf8f67007c80f487ed5dc5ed7a0ac
OliverTrengrove1 <trengroveo2@middleton.school.nz>
2022-05-31 20:39:09 +1200

    f

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ age = get_age()  # 2nd function
 
 print(f'\nHi {name} at {age} years old this will be a piece of cake.\n')
 print('Welcome to the te reo quiz')
-answer = input('Are you ready to play the Quiz ? (yes/no) :')
+answer = input('Are you ready to play the Quiz ? (yes/no) :')  # internal input
 score = 0
-total_questions = 6
+total_questions = 10
 
-if answer.lower() == 'yes'or'y':
-    answer = input('Question 1: What is one in maori \n a.rua \n b.seven \n c.tahi \n d. uno\n')
+if answer == ('yes' or 'y' or 'maybe'):
+    answer = input('Question 1: What is one in maori \n a.rua \n b.seven \n c.tahi \n d.uno\n')
 
     while answer.lower() != 'tahi':
         print('Wrong Answer :(')
@@ -38,7 +38,7 @@ if answer.lower() == 'yes'or'y':
         score += 1
         print('correct')
 
-    answer = input('Question 2:what is sunday in maori?\n a.ratapu\n b.raapa\n c.meircoles\n d.sunday\n ')
+    answer = input('Question 2: what is sunday in maori?\n a.ratapu\n b.raapa\n c.meircoles\n d.sunday\n ')
     while answer.lower() != 'ratapu':
         print('Wrong Answer :(')
         answer = input('Question 2:what is sunday in maori?\n a.ratapu\n b.raapa\n c.meircoles\n d.sunday\n')
@@ -53,6 +53,7 @@ if answer.lower() == 'yes'or'y':
     else:
         score += 1
         print('correct')
+
     answer = input('Question 4: What is five in maori \n a.kiki \n b.drake \n c.5 \n d.rima\n')
     while answer.lower() != 'rima':
         print('Wrong Answer :(')
@@ -60,6 +61,7 @@ if answer.lower() == 'yes'or'y':
     else:
         score += 1
         print('correct')
+
     answer = input('Question 5: What is good morning in te reo \n a.morena \n b.whaka ora \n c.tangato \n d.morning\n')
     while answer.lower() != 'morena':
         print('Wrong Answer :(')
@@ -67,6 +69,7 @@ if answer.lower() == 'yes'or'y':
     else:
         score += 1
         print('correct')
+
     answer = input('Question 6: What is tired in maori \n a.yawny \n b.ngenge \n c.abburido \n d.zzzzzz \n')
     while answer.lower() != 'ngenge':
         print('Wrong Answer :(')
@@ -76,9 +79,44 @@ if answer.lower() == 'yes'or'y':
         score += 1
         print('correct')
 
-elif answer.lower() != 'yes':
-    print('ok thank you for considering')
+    answer = input('Question 7: What is kotahi rau in english? \n a.1000 \n b.78 \n c.100 \n d.1\n')
+    while answer.lower() != '100':
+        print('Wrong Answer :(')
+        answer = input('Question 5: What is kotahi rau in english? \n a.1000 \n b.78 \n c.100 \n d.1\n')
 
+    else:
+        score += 1
+        print('correct')
+
+    answer = input('Question 8: What is the meaning of marae in english?(you can use a letter for this one) \n a.house \n b.home place \n c.cat \n d.a communal or sacred place that serves religious and social purposes in Polynesian societies\n')
+    while answer.lower() != 'd':
+        print('Wrong Answer :(')
+        answer = input('Question 8: What is the meaning of marae in english?(you can use a letter for this one) \n a.house \n b.home place \n c.cat \n d.a communal or sacred place that serves religious and social purposes in Polynesian societies\n')
+
+    else:
+        score += 1
+        print('correct')
+
+    answer = input('Question 9: What is awa in english? \n a.river \n b.love \n c.energy \n d.gathering\n')
+    while answer.lower() != 'river':
+        print('Wrong Answer :(')
+        answer = input('Question 9: What is awa in english? \n a.river \n b.love \n c.energy \n d.gathering\n')
+
+    else:
+        score += 1
+        print('correct')
+
+    answer = input('Question 10: What is pātaitai english? \n a.food \n b.quiz \n c.python \n d.fun\n')
+    while answer.lower() != 'quiz':
+        print('Wrong Answer :(')
+        answer = input('Question 10: What is pātaitai english? \n a.food \n b.quiz \n c.python \n d.fun\n')
+
+    else:
+        score += 1
+        print('correct')
+
+elif answer == ('no'):
+    print('ok thank you for considering')
 
 print('Thanks for playing, you got\n', score, "\nquestions correct!")
 mark = (score / total_questions) * 100
